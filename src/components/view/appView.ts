bc13550175980e556814862bc0ea4e85dd78784b
VaniaToper <vanjaqwe58@gmail.com>
2021-12-06 23:14:14 +0100

    feat🔨: add home page

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,6 +1,14 @@
 import News from './news/news.ts';
 import Sources from './sources/sources.ts';
-
+const startButton = document.querySelector('.start__button');
+const sources = document.querySelector('.sources');
+const news = document.querySelector('.news');
+const home = document.querySelector('.home');
+startButton.addEventListener('click', () => {
+  sources.style.display = 'block';
+  news.style.display = 'block';
+  home.style.display = 'none';
+});
 export class AppView {
   news: News;
   sources: Sources;
